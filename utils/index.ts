fb4aac8b1dc4ca94b62830db94225cbb913c9b0e
juncho154930 <jxc154930@gmail.com>
2023-06-12 03:56:03 -0500

    initial commit

---
+++ utils/index.ts
@@ -0,0 +1,62 @@
+import { CarProps, FilterProps } from "@/types";
+
+export async function fetchCars(filters: FilterProps) {
+  const { manufacturer, year, model, limit, fuel } = filters;
+
+  const headers = {
+    'X-RapidAPI-Key': process.env.RapidAPI || '',
+    'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+  }
+
+  const response = await fetch(
+    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`, 
+    { headers: headers }
+  )
+
+  const result = await response.json()
+
+  return result
+}
+
+export const calculateCarRent = (city_mpg: number, year: number) => {
+  const basePricePerDay = 50; // Base rental price per day in dollars
+  const mileageFactor = 0.1; // Additional rate per mile driven
+  const ageFactor = 0.05; // Additional rate per year of vehicle age
+
+  // Calculate additional rate based on mileage and age
+  const mileageRate = city_mpg * mileageFactor;
+  const ageRate = (new Date().getFullYear() - year) * ageFactor;
+
+  // Calculate total rental rate per day
+  const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+
+  return rentalRatePerDay.toFixed(0);
+};
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+  // TODO - if needed, sign up with COMPANY EMAIL: https://www.imagin.studio/car-image-api  
+  // REMEMBER TO PUT THIS IN A .env FILE
+  // THIS API KEY IS FROM https://gist.github.com/adrianhajdin/e41751d170881f32955f556aaa45c77c "api-key"
+  const url = new URL('https://cdn.imagin.studio/getimage')
+
+  const { make, year, model } = car;
+
+  url.searchParams.append('customer', process.env.imagin || 'hrjavascript-mastery')
+  url.searchParams.append('make', make)
+  url.searchParams.append('modelFamily', model.split(' ')[0])
+  url.searchParams.append('zoomType', 'fullscreen')
+  url.searchParams.append('modelYear', `${year}`)
+  url.searchParams.append('angle', `${angle}`)
+
+  return `${url}`
+}
+
+export const updateSearchParams = (type: string, value:string) => {
+  const searchParams = new URLSearchParams(window.location.search)
+
+  searchParams.set(type, value)
+
+  const newPathname = `${window.location.pathname}?${searchParams.toString()}`
+
+  return newPathname
+}
